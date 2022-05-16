7035305947c6e214ac85c335ac0cad0789dd863b
aliortiz2000 <alinesortiz2000@gmail.com>
2022-05-16 17:10:06 -0400

    Notifier

--- Appointment/AppointmentType/Remote.ts
+++ Appointment/AppointmentType/Remote.ts
@@ -2,15 +2,23 @@ import { Appointment } from "../Appointment";
 import { Observer } from "../../Observer/Observer";
 
 export class Remote extends Appointment {
+  //ATRIBUTO
+  protected via: string;
+  protected link: string;
+
   //CONSTRUCTOR
   constructor(
     date: string,
     time: string,
     status: string,
     doctor: Observer,
-    patient: Observer
+    patient: Observer,
+    via: string,
+    link: string
   ) {
     super(date, time, status, doctor, patient);
+    this.via = via;
+    this.link = link;
   }
 
   //METODO ABSTRACTO

f1809aaa33bfc10169a3c7e4a8dc342b60fb1e5a
aliortiz2000 <alinesortiz2000@gmail.com>
2022-05-16 20:53:22 -0400

    Notifier

--- Doctor/test.ts
+++ Doctor/test.ts
@@ -1,5 +1,1 @@
-import { SpecialitiesName } from "../Specialty/SpecialitiesName";
-import { Specialty } from "../Specialty/Specialty";
-import { Surgeon } from "./DoctorType/Surgeon";
 
-let s = new Surgeon("Nestor Ortiz", new Specialty(SpecialitiesName.Surgeon));
